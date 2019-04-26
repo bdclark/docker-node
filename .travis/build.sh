3356b035ac92d3113886bcff49b110d97b5a0c4b
Brian Clark <brian@clark.zone>
2019-04-26 12:24:16 -0400

    Refactor travis build

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -ex
-
-image_name=
-revision=
+set -e
 
 die() {
    [[ -n "$1" ]] && >&2 echo "Error: $1"
@@ -11,28 +8,27 @@ die() {
 
 [[ -n "$REPO" ]] || die "REPO required"
 [[ -n "$VERSION" ]] || die "VERSION required"
+[[ -n "$DIST" ]] || die "DIST required"
 
-if [ -n "$DIST" ]; then
-  image_name="${REPO}:XXXX-${DIST}"
-  docker build --tag mybuild --build-arg "NODE_VERSION=$VERSION" "$DIST"
-else
-  image_name="${REPO}:XXXX"
-  docker build --tag mybuild --build-arg "NODE_VERSION=$VERSION" .
-fi
+docker build --tag mybuild --build-arg "NODE_VERSION=$VERSION" "$DIST"
 
-revision=$(docker run --rm --entrypoint=node mybuild --version) || die "unable to determine revision"
-revision=$(echo $revision | sed -e 's/^v//') || die "failed to parse revision"
+node_version=$(docker run --rm --entrypoint=node mybuild --version) || die "unable to determine revision"
+node_version="${node_version//v/}"
 
-docker tag mybuild "${image_name/XXXX/$VERSION}"
-docker tag mybuild "${image_name/XXXX/$revision}"
+IFS="." read -ra semver <<< "${node_version}"
+node_major_version="${semver[0]}"
+node_minor_version="${semver[0]}.${semver[1]}"
 
-[[ -n "$MAJOR" ]] && docker tag mybuild "${image_name/XXXX/$MAJOR}"
+docker tag mybuild "${REPO}:${VERSION}-${DIST}"
+docker tag mybuild "${REPO}:${node_version}-${DIST}"
+docker tag mybuild "${REPO}:${node_major_version}-${DIST}"
+docker tag mybuild "${REPO}:${node_minor_version}-${DIST}"
 [[ -n "$TAG" ]] && docker tag mybuild "${REPO}:${TAG}"
 
-if [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push $REPO
+  docker push "$REPO"
 else
   echo "skipping docker push, listing images..."
-  docker image ls
+  docker image ls | grep "$REPO"
 fi
